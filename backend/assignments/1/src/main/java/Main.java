c450c3e7634802279da8aca9fc831301e3336670
brahm-kickdrum <brahmleen.kaur@kickdrumtech.com>
2024-01-15 17:49:58 +0530

    added execute transactions file

--- backend/assignments/1/src/main/java/Main.java
+++ backend/assignments/1/src/main/java/Main.java
@@ -1,8 +1,15 @@
+
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.example.ExecuteTransaction.*;
+import static org.example.TransactionReader.readTransactionsFromFile;
 
 public class Main {
 
-    private static final LoggerFile log = new LoggerFile();
+    private static final LoggerService log = new LoggerService();
 
     public static void main(String[] args) {
 
@@ -10,28 +17,50 @@ public class Main {
 
         String tradersCsvFile = "src/main/resources/traders.csv";
 
-        TraderList traderArray = new TraderList();
-        traderArray.readTraderCSV(tradersCsvFile);
+        TraderList traderArr = new TraderList();
+        traderArr.makeTradersList(tradersCsvFile);
 
-        CoinList coinArr = new CoinList(traderArray);
-        coinArr.readCoinCSV(coinsCsvFile);
+        CoinList coinArr = new CoinList(traderArr);
+        coinArr.makeCoinList(coinsCsvFile);
 
         String targetCoinName = "Bitcoin";
-        Coin targetCoin = coinArr.retrieveCoinDetailsBySymbol(targetCoinName);
+        Coin targetCoin = coinArr.retrieveCoinDetails(targetCoinName);
         if (targetCoin != null) {
             log.logInfo("Coin found: " + targetCoin);
         } else {
             log.logInfo("Coin not found with name: " + targetCoinName);
         }
 
+
         String firstName = "Flo";
         String lastName = "Bookamer";
-        traderArray.retrieveTraderDetails(firstName, lastName);
+        Trader targetTrader = traderArr.retrieveTraderDetails(firstName, lastName);
+        if (targetTrader != null) {
+            log.logInfo("Trader found: " + targetTrader);
+        } else {
+            log.logInfo("Trader not found with name: " + firstName);
+        }
 
         coinArr.getTopNCoins(5);
 
         String filePath = "src/main/resources/small_transaction.json";
-        List<Transaction> transactions = TransactionReader.readTransactionsFromFile(filePath);
+        List<Transaction> transactions = readTransactionsFromFile(filePath);
+
+
+        CountDownLatch latch = new CountDownLatch(transactions.size());
+
+
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            for (Transaction transaction : transactions)
+                executorService.submit(new ExecuteTransaction(transactions, coinArr, latch));
+            executorService.shutdown();
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+
         for (Transaction transaction : transactions) {
             switch (transaction.getType()) {
                 case "BUY":
@@ -52,19 +81,4 @@ public class Main {
         }
 
     }
-    private static void handleBuyTransaction(BuyTransaction buyTransaction, CoinList coinArr) {
-        coinArr.buyCoin(buyTransaction.getCoin(), buyTransaction.getQuantity(), buyTransaction.getWalletAddress());
-    }
-    private static void handleSellTransaction(SellTransaction sellTransaction, CoinList coinArr) {
-        coinArr.sellCoin(sellTransaction.getCoin(), sellTransaction.getQuantity(), sellTransaction.getWalletAddress());
-    }
-
-    private static void handleAddVolumeTransaction(AddVolumeTransaction addVolumeTransaction, CoinList coinArr) {
-        coinArr.addVolume(addVolumeTransaction.getCoin(), addVolumeTransaction.getVolume());
-    }
-
-    private static void handleUpdatePriceTransaction(UpdatePriceTransaction updatePriceTransaction, CoinList coinArr) {
-        coinArr.updatePrice(coinArr.retrieveCoinDetailsBySymbol(updatePriceTransaction.getCoin()), updatePriceTransaction.getPrice());
-    }
-
 }
