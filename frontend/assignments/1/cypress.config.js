c59b23783e35cbe6982111588ac8b5d59fe2f3cc
brahm-kickdrum <brahmleen.kaur@kickdrumtech.com>
2024-02-11 23:48:17 +0530

    added html css files

--- frontend/assignments/1/cypress.config.js
+++ frontend/assignments/1/cypress.config.js
@@ -1,3 +1,38 @@
+// const { defineConfig } = require("cypress");
+// const getCompareSnapshotsPlugin = require("cypress-visual-regression/dist/plugin");
+
+// // Export Cypress configuration using defineConfig function
+// module.exports = defineConfig({
+//   // Trash assets (screenshots, videos) before each test run
+//   // screenshotsFolder : "cypress/screenshots",
+//   trashAssetsBeforeRuns: true,
+
+//   // Disable video recording during test runs
+//   video: false,
+
+//   // Configuration for end-to-end (e2e) tests
+//   e2e: {
+//     // Base URL for your application
+//     baseUrl: "http://127.0.0.1:5501/",
+
+//     // Setup Node events for visual regression testing using cypress-visual-regression plugin
+//     setupNodeEvents(on, config) {
+//       // Integrate the compareSnapshots plugin into Cypress
+//       getCompareSnapshotsPlugin(on, config);
+//     },
+//   },
+//   // Environment variables
+//   env: {
+//     EMAIL: "test@gmail.com",
+//     PASSWORD: "test",
+//     USERNAME: "test-user",
+//     NAME: "test-name",
+//     HOME_PAGE_URL: "index.html",
+//     LOGIN_PAGE_URL: "login/index.html",
+//     REGISTER_PAGE_URL: "/register/index.html",
+//     TEST_THRESHOLD: 0.2,
+//   },
+// });
 const { defineConfig } = require("cypress");
 const getCompareSnapshotsPlugin = require("cypress-visual-regression/dist/plugin");
 
@@ -13,7 +48,7 @@ module.exports = defineConfig({
   // Configuration for end-to-end (e2e) tests
   e2e: {
     // Base URL for your application
-    baseUrl: "http://localhost:5500/assignments/1",
+    baseUrl: "http://localhost:5501/",
 
     // Setup Node events for visual regression testing using cypress-visual-regression plugin
     setupNodeEvents(on, config) {
@@ -27,9 +62,9 @@ module.exports = defineConfig({
     PASSWORD: "test",
     USERNAME: "test-user",
     NAME: "test-name",
-    HOME_PAGE_URL: "/home-page/index.html",
-    LOGIN_PAGE_URL: "login/index.html",
-    REGISTER_PAGE_URL: "/register/index.html",
+    HOME_PAGE_URL: "index.html",
+    LOGIN_PAGE_URL: "index.html",
+    REGISTER_PAGE_URL: "index.html",
     TEST_THRESHOLD: 0.35,
   },
 });
